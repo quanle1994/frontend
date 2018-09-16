4e5079b0264231e0f3e85f9ba074fbed9592aa98
handshou <heyhansel@outlook.com>
2018-09-16 13:06:27 +0800

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+)
+
+const Login = () => (
+  <div>
+    <h1>Login</h1>
+  </div>
+)
+
+const Nav = () => {
+  return (
+    <div>
+      <Link to="/">
+        <button className="btn">Home</button>
+      </Link>
+      <Link to="/login">
+        <button className="btn">Login</button>
+      </Link>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome</h1>
+              <Nav />
+          </header>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default App
