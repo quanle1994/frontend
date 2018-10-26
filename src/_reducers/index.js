c936610ebfc48c91ad3a24db1ab56f017d30cfd1
Quan Le <quan_le@u.nus.edu>
2018-10-26 23:17:40 +0800

    Edit layouts

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -4,12 +4,16 @@ import { authentication } from './authentication.reducer';
 import { registration } from './registration.reducer';
 import { users } from './users.reducer';
 import { alert } from './alert.reducer';
+import currentPage from './currentPage.reducer';
+import userProfile from './userProfile.reducer';
 
 const rootReducer = combineReducers({
   authentication,
   registration,
   users,
-  alert
+  alert,
+  currentPage,
+  userProfile,
 });
 
 export default rootReducer;
