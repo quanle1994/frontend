dfae02602ed4e85cba72380b164c4ae5d6696444
Quan Le <quan_le@u.nus.edu>
2018-11-28 18:03:23 +0800

    Fix host to localhost

--- src/_api/constants.js
+++ src/_api/constants.js
@@ -1,6 +1,6 @@
 // export const BACKEND_SERVER = 'http://172.19.195.190:8080/Qoodie-war/Resource';
-// export const BACKEND_HOST = 'http://localhost:8080/';
+export const BACKEND_HOST = 'http://localhost:8080/';
 // export const BACKEND_HOST = 'http://172.19.195.190:8080/';
-export const BACKEND_HOST = 'http://172.19.195.190:8080/';
+// export const BACKEND_HOST = 'http://172.19.195.190:8080/';
 
 export const BACKEND_SERVER = `${BACKEND_HOST}Qoodie-war/Resource`;
